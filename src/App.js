86870b108024278da168c986995ed2682ccc138e
jonathanhovland <jonathan.hovland@yahoo.com>
2019-10-11 08:49:17 -0600

    added bootsrtap navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react"
 import "./App.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
 // Component imports
 import Home from "./components/Home"
 import Navigation from "./components/Navigation"
-import Tasks from "./components/Tasks"
 import Family from "./components/Family"
+import Tasks from "./components/Tasks"
 import Calendar from "./components/Calendar"
 
 function App() {
@@ -16,8 +17,8 @@ function App() {
       <Route path="/" component={Navigation}/>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/tasks" component={Tasks}/>
         <Route path="/family" component={Family}/>
+        <Route exact path="/tasks" component={Tasks}/>
         <Route path="/calendar" component={Calendar}/>
       </Switch>
     </Router>
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,16 +1,40 @@
 import React from "react"
 import logo from "../../imgages/logo.jpg"
+import Navbar from 'react-bootstrap/Navbar'
+import Button from 'react-bootstrap/Button'
+import Nav from 'react-bootstrap/Nav'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+
+
+
+
 import { Link } from "react-router-dom"
 
 const Navigation = () => {
     return (
-        <nav>
-            <img src={logo} width={100} height={75} mode='fit' alt="logo" />
-            <Link to="/">Home</Link>
-            <Link to="/tasks">Tasks</Link>
-            <Link to="/family">Family</Link>
-            <Link to="/calendar">Calendar</Link>
-        </nav>
+        // <nav>
+        //     <img src={logo} width={100} height={75} mode='fit' alt="logo" />
+        //     <Link to="/">Home</Link>
+        //     <Link to="/family">Family</Link>
+        //     <Link to="/tasks">Tasks</Link>
+        //     <Link to="/calendar">Calendar</Link>
+        // </nav>
+        <>
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand>goobbr</Navbar.Brand>
+            <Nav className="mr-auto">
+                <Link to="/">Home</Link>
+                <Link to="/family">Family</Link>
+                <Link to="/tasks">Tasks</Link>
+                <Link to="/calendar">Calendar</Link>
+            </Nav>
+            <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-info">Search</Button>
+            </Form>
+        </Navbar>
+        </>
     )
 }
 
