0769353b4db689be0c515f97a385256ce3bce0f8
jonathanhovland <jonathan.hovland@yahoo.com>
2019-10-11 09:24:18 -0600

    updated look of navbar

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,40 +1,28 @@
 import React from "react"
-import logo from "../../imgages/logo.jpg"
+
 import Navbar from 'react-bootstrap/Navbar'
 import Button from 'react-bootstrap/Button'
 import Nav from 'react-bootstrap/Nav'
 import Form from 'react-bootstrap/Form'
 import FormControl from 'react-bootstrap/FormControl'
 
-
-
-
 import { Link } from "react-router-dom"
 
 const Navigation = () => {
     return (
-        // <nav>
-        //     <img src={logo} width={100} height={75} mode='fit' alt="logo" />
-        //     <Link to="/">Home</Link>
-        //     <Link to="/family">Family</Link>
-        //     <Link to="/tasks">Tasks</Link>
-        //     <Link to="/calendar">Calendar</Link>
-        // </nav>
-        <>
         <Navbar bg="dark" variant="dark">
-            <Navbar.Brand>goobbr</Navbar.Brand>
+            <Navbar.Brand style={{color: '#52C7CC', fontSize: '35px' }}>goobbr</Navbar.Brand>
             <Nav className="mr-auto">
-                <Link to="/">Home</Link>
-                <Link to="/family">Family</Link>
-                <Link to="/tasks">Tasks</Link>
-                <Link to="/calendar">Calendar</Link>
+                <Link style={{ textDecoration: 'none', color: 'white', padding: 20, marginTop: 10 }} to="/">Home</Link>
+                <Link style={{ textDecoration: 'none', color: 'white', padding: 20, marginTop: 10 }} to="/family">Family</Link>
+                <Link style={{ textDecoration: 'none', color: 'white', padding: 20, marginTop: 10 }} to="/tasks">Tasks</Link>
+                <Link style={{ textDecoration: 'none', color: 'white', padding: 20, marginTop: 10 }} to="/calendar">Calendar</Link>
             </Nav>
             <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-info">Search</Button>
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <Button style={{color: '#52C7CC'}} variant="outline-info">Search</Button>
             </Form>
         </Navbar>
-        </>
     )
 }
 
